43f46217fc5db5a8aa622a10a451f1d5a9d3371b
cookies-xor-cream <aryageza@gmail.com>
2022-11-15 14:43:07 +0800

    Added Students to the admin panel

--- yoseikan_karate/yoseikan_app/models.py
+++ yoseikan_karate/yoseikan_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Student(models.Model):
+    student_id = models.CharField(max_length=512)
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     birth_date = models.CharField(max_length=128)
+    address = models.CharField(max_length=512)
+    join_date = models.CharField(max_length=128)
     validated = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.student_id})'
